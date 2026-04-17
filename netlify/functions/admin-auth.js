bfabb257d6e709bfee33cf0e6e3478bca9eafe27
amanp-wq <aman.p@elevateme.pro>
2026-04-17 14:29:17 -0400

    Update admin-auth.js

--- netlify/functions/admin-auth.js
+++ netlify/functions/admin-auth.js
@@ -3,7 +3,7 @@ const { createClient } = require('@supabase/supabase-js');
 const SUPABASE_URL = 'https://vvazzmoplwfubfhllnwf.supabase.co';
 // This key is safe to include here as it is a server-side function
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2YXp6bW9wbHdmdWJmaGxsbndmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYwOTI1NjMsImV4cCI6MjA5MTY2ODU2M30.pZYjPTsi5Km5OpI02MQMyPEUW9eTLaCJt8cDkFzH05o';
-const ADMIN_EMAILS = ['support@elevateme.pro', 'divina.r@elevateme.pro', 'aman.p@elevateme.pro'];
+const ADMIN_EMAILS = ['support@elevateme.pro', 'divina.r@elevateme.pro', 'aman.p@elevateme.pro', 'nitti.v@elevateme.pro'];
 
 exports.handler = async (event) => {
     const headers = {
